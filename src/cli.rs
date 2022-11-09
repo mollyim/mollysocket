4c8d045d97745f272b78f8e28fa03722b11640d4
sim <git@sgougeon.fr>
2022-11-09 22:43:59 +0100

    Move cli commands to cli mod

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,32 @@
-pub mod connection;
-pub mod oneshot;
-pub mod server;
+use std::env;
+
+mod connection;
+mod oneshot;
+mod server;
+
+fn usage() {
+    println!(
+        "
+Usage: {0} [command] [args, ...]
+
+Commands:
+  oneshot      Connect to a websocket and push to the endpoint
+  server       Run webserver and websockets
+  connection    List, add and remove connections
+
+Run '{0} [command] --help' for more information on a command.
+",
+        env::args().nth(0).unwrap()
+    );
+}
+
+pub async fn cli() {
+    let mut args = env::args();
+    args.next();
+    match args.next() {
+        Some(cmd) if cmd == "oneshot" || cmd == "o" => oneshot::oneshot(args).await,
+        Some(cmd) if cmd == "connection" || cmd == "c" => connection::connection(args),
+        Some(cmd) if cmd == "server" || cmd == "s" => server::server(args).await,
+        _ => usage(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use lazy_static::lazy_static;
-use std::env;
 
 use config::Config;
 
@@ -13,32 +12,9 @@ lazy_static! {
     static ref CONFIG: Config = Config::load();
 }
 
-fn usage() {
-    println!(
-        "
-Usage: {0} [command] [args, ...]
-
-Commands:
-  oneshot      Connect to a websocket and push to the endpoint
-  server       Run webserver and websockets
-  connection    List, add and remove connections
-
-Run '{0} [command] --help' for more information on a command.
-",
-        env::args().nth(0).unwrap()
-    );
-}
-
 #[tokio::main]
 async fn main() {
     env_logger::init();
     // dbg!(&*CONFIG);
-    let mut args = env::args();
-    args.next();
-    match args.next() {
-        Some(cmd) if cmd == "oneshot" || cmd == "o" => cli::oneshot::oneshot(args).await,
-        Some(cmd) if cmd == "connection" || cmd == "c" => cli::connection::connection(args),
-        Some(cmd) if cmd == "server" || cmd == "s" => cli::server::server(args).await,
-        _ => usage(),
-    }
+    cli::cli().await;
 }
