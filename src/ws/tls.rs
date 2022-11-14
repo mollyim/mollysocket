3fbfca89976e980f83887b318b6fa65de6fe0833
sim <git@sgougeon.fr>
2022-11-14 23:33:24 +0100

    Pin signal CA

--- src/ws/tls.rs
+++ src/ws/tls.rs
@@ -1,7 +1,31 @@
-use native_tls;
+use native_tls::{Certificate, TlsConnector};
 
-pub fn build_tls_connector() -> Result<native_tls::TlsConnector, native_tls::Error> {
-    let mut builder = native_tls::TlsConnector::builder();
-    builder.danger_accept_invalid_certs(true);
+pub fn build_tls_connector() -> Result<TlsConnector, native_tls::Error> {
+    let root_ca = include_bytes!("certs/signal-messenger.pem");
+    let root_ca = Certificate::from_pem(root_ca).unwrap();
+    let mut builder = TlsConnector::builder();
+    builder.disable_built_in_roots(true);
+    builder.add_root_certificate(root_ca);
     builder.build()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::TcpStream;
+
+    use super::*;
+
+    #[test]
+    fn connect_trusted_server() {
+        let builder = build_tls_connector().unwrap();
+        let s = TcpStream::connect("chat.staging.signal.org:443").unwrap();
+        builder.connect("chat.staging.signal.org", s).unwrap();
+    }
+
+    #[test]
+    fn connect_untrusted_server() {
+        let builder = build_tls_connector().unwrap();
+        let s = TcpStream::connect("signal.org:443").unwrap();
+        builder.connect("signal.org", s).unwrap_err();
+    }
 }
