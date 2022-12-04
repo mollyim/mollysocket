b0e1350f1559a25546de3d7ab319586d08f74819
sim <git@sgougeon.fr>
2022-12-04 15:40:48 +0100

    Stop server with ^C

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 use crate::db::MollySocketDb;
-use futures_util::{join, FutureExt};
+use futures_util::{future::join, pin_mut, select, FutureExt};
 use lazy_static::lazy_static;
 use std::sync::{Arc, Mutex};
+use tokio::signal;
 
 mod connections;
 mod web;
@@ -13,5 +14,13 @@ lazy_static! {
 }
 
 pub async fn run() {
-    join!(web::launch().fuse(), connections::run().fuse());
+    let signal_future = signal::ctrl_c().fuse();
+    let joined_future = join(web::launch().fuse(), connections::run().fuse());
+
+    pin_mut!(signal_future, joined_future);
+
+    select!(
+        _ = signal_future => log::info!("SIGINT received"),
+        _ = joined_future => log::warn!("Server stopped"),
+    )
 }
